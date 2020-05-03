7f4ce9337d71e04be36b9b26ed797c210f077e74
M.Emre YILMAZ <memreyilmaz@gmail.com>
2020-05-03 19:36:00 +0300

    Menu and action mode menus added BookAdapter selection methods added MainActivity menu methods added

--- app/src/main/java/com/example/recylerviewmultiselection/MainActivity.kt
+++ app/src/main/java/com/example/recylerviewmultiselection/MainActivity.kt
@@ -2,20 +2,29 @@ package com.example.recylerviewmultiselection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var actionMode: ActionMode? = null
+    private lateinit var bookAdapter: BookAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerview.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerview.adapter = BookAdapter(getBooks())
+        bookAdapter = BookAdapter(onItemClickListener,onItemLongClickListener, onQuantityClickListener)
+        bookAdapter.setBookList(getBooks())
+        recyclerview.adapter = bookAdapter
     }
 
-    fun getBooks(): MutableList<Book> {
+    private fun getBooks(): MutableList<Book> {
         return mutableListOf(
             Book(id = 1,cover = R.drawable.annakarenina,quantity = 1, name = "Anna Karenina", price = 13.45),
             Book(id = 2,cover = R.drawable.madamebovary,quantity = 2, name = "Madame Bovary", price = 4.99),
@@ -28,5 +37,111 @@ class MainActivity : AppCompatActivity() {
             Book(id = 9,cover = R.drawable.thestoriesofantonchekhov,quantity = 1, name = "The Stories of Anton Chekhov", price = 12.10),
             Book(id = 10,cover = R.drawable.middlemarch,quantity = 5, name = "Middlemarch", price = 7.99)
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_edit, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_edit -> {
+                if (actionMode == null) {
+                    actionMode = this.startActionMode(ActionModeCallback())
+                    bookAdapter.changeCheckboxVisibility()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    open inner class ActionModeCallback : ActionMode.Callback {
+        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+            val count = bookAdapter.getSelectedBooksCount()
+            when (item?.itemId) {
+                R.id.action_delete_from_cart -> {
+                    if (count > 0) {
+                        bookAdapter.deleteSelectedBooks()
+                        Toast.makeText(
+                            this@MainActivity,
+                            count.toString() + "Item(s) Deleted From Your Cart",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        mode?.finish()
+                    }
+                    return true
+                }
+                R.id.action_add_to_favourites_list -> {
+                    if (count > 0) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            count.toString() + "Item(s) Added To Your Favourites List",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        mode?.finish()
+                    }
+                    return true
+                }
+            }
+            return true
+        }
+
+        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            mode?.menuInflater?.inflate(R.menu.menu_action_mode, menu)
+            mode?.title = "Select"
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            return false
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode?) {
+            bookAdapter.changeCheckboxVisibility()
+            bookAdapter.clearSelectedBooksList()
+            actionMode?.subtitle = ""
+            actionMode = null
+        }
+    }
+
+    private val onItemClickListener = { view: View?, position: Int ->
+        if (actionMode != null) {
+            toggleSelection(position)
+        }
+    }
+
+    private val onItemLongClickListener = { view: View?, position: Int ->
+        if (actionMode != null) {
+            toggleSelection(position)
+        } else {
+            actionMode = this.startActionMode(ActionModeCallback())
+            bookAdapter.changeCheckboxVisibility()
+            toggleSelection(position)
+        }
+    }
+
+    private val onQuantityClickListener: ((Book) -> Unit) = {book->
+        Toast.makeText(
+            this@MainActivity,
+            book.name +" Quantity Button Clicked",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun toggleSelection(position: Int) {
+        bookAdapter.toggleSelection(position)
+        val count: Int = bookAdapter.getSelectedBooksCount()
+        if (count == 0) {
+            actionMode?.finish()
+        } else {
+            actionMode?.apply {
+                title = resources.getQuantityString(R.plurals.title_action_bar_item_selected, count, count)
+                menu.findItem(R.id.action_delete_from_cart).setIcon(R.drawable.ic_delete_active)
+                menu.findItem(R.id.action_add_to_favourites_list).setIcon(R.drawable.ic_favorite_active)
+                subtitle = getString(R.string.cart_sum, bookAdapter.getCartTotalSum())
+                invalidate()
+            }
+        }
     }
 }
